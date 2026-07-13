827fbd60693a6387886924f7952b146afea104e0
will wade <willwade@gmail.com>
2026-07-13 05:54:05 +0100

    Bump version to 0.4.2 for upgrade detection

--- scripts/build-setup.ps1
+++ scripts/build-setup.ps1
@@ -53,7 +53,7 @@ if ([string]::IsNullOrWhiteSpace($Version)) {
         }
     }
     if ([string]::IsNullOrWhiteSpace($Version)) {
-        $Version = "0.4.1.0"
+        $Version = "0.4.2.0"
     }
 }
 
